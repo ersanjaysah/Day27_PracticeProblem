dc520a796599d7988695fedb7e1e77a200edce11
sanjay <ssah6694@gmail.com>
2022-04-10 16:13:51 +0530

    [sanjay]Added: address problem file

---
+++ File_IO/File_IO/Person.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_IO
+{
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public string Address;
+        public long PhoneNumber;
+
+        public string Result()
+        {
+            return " Name is: " + FirstName + " " + LastName + "\n Address: " + Address + "\n Phone: " + PhoneNumber;
+        }
+    }
+
+    public class AddressBook
+    {
+
+        public List<Person> person = new List<Person>(); //creating list
+        public AddressBook()  //Address Book method
+        {
+            string json = File.ReadAllText(@"E:\BridgeLabzAssignment\Day27_Problems\File_IO\File_IO\File.json");
+            if (json.Length > 0)
+            {
+                person = JsonConvert.DeserializeObject<List<Person>>(json);
+            }
+            else
+                person = new List<Person>();
+        }
+
+        public void Display()  //Display Method
+        {
+            if (person.Count == 0)
+            {
+                Console.WriteLine("Please add Some Contact list First");
+            }
+            Console.WriteLine("Welcome to Program");
+            foreach (Person per in person)
+            {
+                Console.WriteLine(per.Result());
+            }
+        }
+
+        public void addPerson(Person p)  //Adding Contact list method
+        {
+            person.Add(p);
+            string jsonData = JsonConvert.SerializeObject(person);
+            File.WriteAllText(@"E:\BridgeLabzAssignment\Day27_Problems\File_IO\File_IO\File.json", jsonData);
+        }
+    }
+}
--- File_IO/File_IO/Program.cs
+++ File_IO/File_IO/Program.cs
@@ -7,6 +7,47 @@ namespace File_IO
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the address book problems");
+
+            AddressBook manager = new AddressBook();
+
+            while (true)
+            {
+                Console.WriteLine("\nPlease Select any one option: ");
+                Console.WriteLine("Select 1: For add Details to Address Book");
+                Console.WriteLine("select 2: For Display All Contacts of Address Book");
+                int read = int.Parse(Console.ReadLine());
+                switch (read)
+                {
+                    case 1:        //For Adding Details to Address Book
+                        {
+                            Person person = new Person();
+                            Console.WriteLine("Enter First Name: ");
+                            string firstname = Console.ReadLine();
+                            person.FirstName = firstname;
+                            Console.WriteLine("Enter Last Name: ");
+                            string lastname = Console.ReadLine();
+                            person.LastName = lastname;
+                            Console.WriteLine("Enter Address: ");
+                            string address = Console.ReadLine();
+                            person.Address = address;
+                            Console.WriteLine("Enter Phone Number: ");
+                            long number = long.Parse(Console.ReadLine());
+                            person.PhoneNumber = number;
+                            manager.addPerson(person);
+                        }
+                        break;
+                    case 2:    //Display contact list of addressbook
+                        {
+                            manager.Display();
+                        }
+                        break;
+                    default:
+                        {
+                            Console.WriteLine("Please Select any one operation ");
+                        }
+                        break;
+                }
+            }
         }
     }
 }
