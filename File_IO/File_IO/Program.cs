3aeb5720728387bda161c0d84a69b89326205bd1
sanjay <ssah6694@gmail.com>
2022-04-10 14:44:18 +0530

    [sanjay]Added: welcome file

---
+++ File_IO/File_IO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace File_IO
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the address book problems");
+        }
+    }
+}
